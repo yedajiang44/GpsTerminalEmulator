d8a91d76ed159a0a8d3ef8a942e9099e25a87f1b
yedajiang44 <602830483@qq.com>
2020-07-11 15:16:49 +0800

    移动命名空间

---
+++ src/Jt808TerminalEmulator.Core/Netty/Codec/Jt808TcpMetadataDecoder .cs
@@ -0,0 +1,37 @@
+﻿using DotNetty.Buffers;
+using DotNetty.Codecs;
+using DotNetty.Transport.Channels;
+using GpsPlatform.Infrastructure.Extentions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jt808TerminalEmulator.Core.Netty.Codec
+{
+    internal class Jt808Decoder : ByteToMessageDecoder
+    {
+        private readonly ILogger logger;
+        public Jt808Decoder(ILogger<Jt808Decoder> logger)
+        {
+            this.logger = logger;
+        }
+        protected override void Decode(IChannelHandlerContext context, IByteBuffer input, List<object> output)
+        {
+            if (input.Capacity >= 12)
+            {
+                byte[] buffer = new byte[input.Capacity + 2];
+                input.ReadBytes(buffer, 1, input.Capacity);
+                buffer[0] = 0x7e;
+                buffer[input.Capacity + 1] = 0x7e;
+                output.Add(buffer);
+            }
+            else
+            {
+                input.SetReaderIndex(int.MaxValue);
+                if (logger.IsEnabled(LogLevel.Error))
+                    logger.LogError($"收到无效数据包：{input.ReadBytes(input.Capacity).Array.ToHexString()}");
+            }
+        }
+    }
+}
---
+++ src/Jt808TerminalEmulator.Core/Netty/Codec/Jt808TcpMetadataEncoder .cs
@@ -0,0 +1,40 @@
+﻿using DotNetty.Buffers;
+using DotNetty.Codecs;
+using DotNetty.Transport.Channels;
+using GpsPlatform.Jt808Protocol;
+using GpsPlatform.Jt808Protocol.PackageInfo;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jt808TerminalEmulator.Core.Netty.Codec
+{
+    internal class Jt808Encoder : MessageToByteEncoder<Jt808PackageInfo>
+    {
+        readonly ILogger logger;
+        readonly PackageConverter packageConverter;
+
+        public Jt808Encoder(ILogger<Jt808Encoder> logger, PackageConverter packageConverter)
+        {
+            this.logger = logger;
+            this.packageConverter = packageConverter;
+        }
+
+        protected override void Encode(IChannelHandlerContext context, Jt808PackageInfo message, IByteBuffer output)
+        {
+            if (message != null)
+            {
+                try
+                {
+                    output.WriteBytes(Unpooled.WrappedBuffer(packageConverter.Serialize(message)));
+                }
+                catch (Exception e)
+                {
+                    if (logger.IsEnabled(LogLevel.Error))
+                        logger.LogError(e, $"消息 {message.Header.MessageId:X2} 编码发生异常");
+                }
+            }
+        }
+    }
+}
