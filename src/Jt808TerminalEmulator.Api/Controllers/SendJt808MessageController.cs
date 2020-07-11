e3ed89a195151760d3f658d7e77f8de1db98effc
yedajiang44 <602830483@qq.com>
2020-07-11 15:19:02 +0800

    添加测试

---
+++ src/Jt808TerminalEmulator.Api/Controllers/SendJt808MessageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using GpsPlatform.Jt808Protocol.Instruction;
+using GpsPlatform.Jt808Protocol.PackageInfo;
+using Jt808TerminalEmulator.Core;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Jt808TerminalEmulator.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SendJt808MessageController : ControllerBase
+    {
+        private readonly ITcpClientFactory tcpClientFactory;
+
+        public SendJt808MessageController(ITcpClientFactory tcpClientFactory)
+        {
+            this.tcpClientFactory = tcpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAsync()
+        {
+            try
+            {
+                var client = await tcpClientFactory.CreateTcpClient();
+                await client.ConnectAsync("127.0.0.1", 808);
+                var package = new Jt808PackageInfo { Header = new Header { PhoneNumber = "13800138000" }, Body = new Jt808_0x0002_Heartbeat() };
+                await client.Send(package);
+                return Ok(new { flag = true, data = package });
+            }
+            catch (Exception e)
+            {
+                return Ok(new { flag = false, message = e.Message });
+            }
+        }
+    }
+}
--- src/Jt808TerminalEmulator.Api/Startup.cs
+++ src/Jt808TerminalEmulator.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Jt808TerminalEmulator.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace Jt808TerminalEmulator.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.UseJt808TerminalEmulator();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
