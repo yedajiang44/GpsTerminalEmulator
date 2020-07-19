e6ba69c2218340c7d9be6a42e88d28c4d79fbad6
yedajiang44 <602830483@qq.com>
2020-07-19 21:25:36 +0800

    添加统一返回结果dto

---
+++ src/Jt808TerminalEmulator.Model/Dtos/JsonResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jt808TerminalEmulator.Model.Dtos
+{
+    public class JsonResultDto
+    {
+        public bool Flag { get; set; }
+        public dynamic Data { get; set; }
+        public string Message { get; set; }
+    }
+}
