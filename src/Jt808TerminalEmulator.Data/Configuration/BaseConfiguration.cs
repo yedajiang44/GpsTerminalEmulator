9d3ff9c866d23aa126ce6acd777c3eb13ed01251
yedajiang44 <602830483@qq.com>
2022-10-30 14:52:40 +0800

    修改数据时忽略创建时间

--- src/Jt808TerminalEmulator.Data/Configuration/BaseConfiguration.cs
+++ src/Jt808TerminalEmulator.Data/Configuration/BaseConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Jt808TerminalEmulator.Data.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
@@ -15,12 +16,13 @@ namespace Jt808TerminalEmulator.Data.Configuration
         public virtual void Configure(EntityTypeBuilder<T> builder)
         {
             //配置全局过滤器
-            builder.HasQueryFilter(x => x.IsDeleted == false);
+            builder.HasQueryFilter(x => !x.IsDeleted);
 
             //配置默认的主键和主键的数据长度
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).HasMaxLength(36);
             builder.Property(x => x.CreateUserId).HasMaxLength(36);
+            builder.Property(x => x.CreateDateTime).Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
         }
     }
 }
