10214cf6a9aeb6d14f05615f885d9226678e6d30
yedajiang44 <602830483@qq.com>
2023-02-23 17:01:35 +0800

    添加数据迁移文件

---
+++ src/Jt808TerminalEmulator.Data/Migrations/20220530160243_Init.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jt808TerminalEmulator.Data.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Line",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Distance = table.Column<decimal>(type: "numeric", nullable: false),
+                    LocationCount = table.Column<int>(type: "integer", nullable: false),
+                    Note = table.Column<string>(type: "text", nullable: true),
+                    CreateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreateUserId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Line", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Terminal",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Sim = table.Column<string>(type: "text", nullable: true),
+                    LicensePlate = table.Column<string>(type: "text", nullable: true),
+                    CreateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreateUserId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Terminal", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Order = table.Column<int>(type: "integer", nullable: false),
+                    Logintude = table.Column<double>(type: "double precision", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    LineId = table.Column<string>(type: "character varying(36)", nullable: true),
+                    CreateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreateUserId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Location_Line_LineId",
+                        column: x => x.LineId,
+                        principalTable: "Line",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Interval = table.Column<int>(type: "integer", nullable: false),
+                    Speed = table.Column<double>(type: "double precision", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Ip = table.Column<string>(type: "text", nullable: true),
+                    Port = table.Column<int>(type: "integer", nullable: false),
+                    LineId = table.Column<string>(type: "character varying(36)", nullable: false),
+                    CreateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreateUserId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Task_Line_LineId",
+                        column: x => x.LineId,
+                        principalTable: "Line",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskTerminals",
+                columns: table => new
+                {
+                    TasksId = table.Column<string>(type: "character varying(36)", nullable: false),
+                    TerminalsId = table.Column<string>(type: "character varying(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskTerminals", x => new { x.TasksId, x.TerminalsId });
+                    table.ForeignKey(
+                        name: "FK_TaskTerminals_Task_TasksId",
+                        column: x => x.TasksId,
+                        principalTable: "Task",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskTerminals_Terminal_TerminalsId",
+                        column: x => x.TerminalsId,
+                        principalTable: "Terminal",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_LineId",
+                table: "Location",
+                column: "LineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_LineId",
+                table: "Task",
+                column: "LineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskTerminals_TerminalsId",
+                table: "TaskTerminals",
+                column: "TerminalsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropTable(
+                name: "TaskTerminals");
+
+            migrationBuilder.DropTable(
+                name: "Task");
+
+            migrationBuilder.DropTable(
+                name: "Terminal");
+
+            migrationBuilder.DropTable(
+                name: "Line");
+        }
+    }
+}
